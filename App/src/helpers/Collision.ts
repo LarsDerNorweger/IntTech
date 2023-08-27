ed4995ec3ccdbd94d38da4d34db892ae113b0047
LarsDerNorweger <boettger.colin@gmail.com>
2023-08-27 10:31:51 +0200

    clean Up

--- App/src/helpers/Collision.ts
+++ App/src/helpers/Collision.ts
@@ -4,8 +4,8 @@
 /*  Autoren: Colin Böttger
 /*------------------------------------------------*/
 
-import { Area, area } from "./Area";
-import { Vektor } from "./Vektor";
+import { Area, area } from "./Area.js";
+import { Vektor } from "./Vektor.js";
 
 export function isColliding(area1: area, area2: area): boolean {
   // if (Area.area(area1) == 0 || Area.area(area2))
@@ -14,15 +14,9 @@ export function isColliding(area1: area, area2: area): boolean {
   let r1 = Vektor.add(area1.begin, area1.size)
   let l2 = area2.begin
   let r2 = Vektor.add(area2.begin, area2.size)
-  console.log(area1, area2, l1, r1, l2, r2)
-  if (l1[0] > r2[0] || l2[0] > r1[0]) {
-    console.log(l1[0] > r2[0] || l2[0] > r1[0], l1[0], r2[0], l2[0], r1[0], "x")
+  if (l1[0] > r2[0] || l2[0] > r1[0])
     return false
-  }
-  if (r1[1] < l2[1] || r2[1] < l1[1]) {
-    console.log(r1[1] > l2[1] || r2[1] > l1[1], r1[1], l2[1], r2[1], l1[1], "y")
+  if (r1[1] < l2[1] || r2[1] < l1[1])
     return false
-  }
-  console.log("true")
   return true
 }
