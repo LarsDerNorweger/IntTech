0a20c5c8629f3628293558534c4230d3ad5e974a
LarsDerNorweger <boettger.colin@gmail.com>
2023-08-30 17:25:01 +0200

    add dom helper

---
+++ App/src/helpers/dom.ts
@@ -0,0 +1,20 @@
+/*------------------------------------------------
+/*  BA-Glauchau Beleg Internettechnologien
+/*
+/*  Autoren: Colin Böttger
+/*------------------------------------------------*/
+
+import { transform } from "typescript"
+
+export{
+create
+}
+
+function create<K extends keyof HTMLElementTagNameMap>(tagname:K,target?:HTMLElement,text?:string): HTMLElementTagNameMap[K]{
+    let res = document.createElement(tagname)
+    if(target)
+        target.appendChild(res)
+    if(text)
+        res.innerText = text
+    return res
+}
