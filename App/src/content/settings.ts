eb74cb384ad63a81c786de23dc761dfba6a70571
LarsDerNorweger <boettger.colin@gmail.com>
2023-10-27 18:19:12 +0200

    implement further settings

--- App/src/content/settings.ts
+++ App/src/content/settings.ts
@@ -1,6 +1,6 @@
 import { vektor } from "../helpers/Vektor.js";
 import { create, createText, role } from "../helpers/dom.js";
-import { createButton } from "../helpers/picoCss.js";
+import { createButton, createSlider } from "../helpers/picoCss.js";
 
 export interface settings
 {
@@ -17,17 +17,20 @@ export function createSettings(onSave: (val: settings) => void, preVal: settings
 
   createButton('', () => mod.open = false, a, false, 'close', 'secondary');
 
-  let sl = createObstacleSlider(a, preVal.obstacleCount);
+  let oCount = createSlider('Anzahl der Hindernisse', (x) => { }, 5, 50, a);
+  oCount.setValue(preVal.obstacleCount);
+  let oSize = createSlider('Breite der Hindernisse', (x) => { }, 50, 500, a);
+  oSize.setValue(preVal.obstaclewitdth);
+
   create('input', a);
   let g = create('footer', a, 'grid');
   createButton('Abbrechen', () => mod.open = false, g, false, 'secondary');
   createButton('Anwenden', () =>
   {
     mod.open = false;
-    onSave({ ...preVal, ...{ obstacleCount: Number.parseInt(sl.value) } });
+    onSave({ ...preVal, ...{ obstaclewitdth: oSize.value(), obstacleCount: oCount.value() } });
   }, g
   );
-
   return mod;
 }
 
--- App/src/helpers/picoCss.ts
+++ App/src/helpers/picoCss.ts
@@ -2,7 +2,8 @@ import { create, role } from "./dom.js";
 
 export
 {
-  createButton
+  createButton,
+  createSlider
 };
 
 
@@ -18,3 +19,41 @@ function createButton(text: string, onclick: () => void, target?: HTMLElement, b
   res.onclick = onclick;
   return res;
 }
+
+function createSlider(text: string, oninput: (value: number) => void, min: number, max: number, target: HTMLElement)
+{
+  let node = create('div', target);
+  let l = create('label', node);
+  let i = id++ + '_slider';
+  l.setAttribute('for', i);
+  let s = create('input', node);
+  s.type = 'range';
+  s.id = i;
+  s.min = min + '';
+  s.max = max + '';
+  setText();
+  s.onchange = () =>
+  {
+    setText();
+    oninput(Number.parseInt(s.value));
+  };
+
+  return {
+    node: node,
+    value: () => Number.parseInt(s.value),
+    setValue: (x: number) =>
+    {
+      s.value = x + '';
+      setText();
+    },
+    min: (x: number) => s.min = x + '',
+    max: (x: number) => s.max = x + ''
+  };
+
+  function setText()
+  {
+    l.innerText = text + ' ' + s.value;
+  }
+}
+
+let id = 0;
