30126c5f46afc43468021ccbfbb7f5fc6e7afe06
LarsDerNorweger <boettger.colin@gmail.com>
2023-10-28 09:33:17 +0200

    expand setting

--- App/src/content/settings.ts
+++ App/src/content/settings.ts
@@ -1,6 +1,6 @@
-import { vektor } from "../helpers/Vektor.js";
+import { Vektor, vektor } from "../helpers/Vektor.js";
 import { create, createText, role } from "../helpers/dom.js";
-import { createButton, createSlider } from "../helpers/picoCss.js";
+import { createAcordion, createButton, createSlider } from "../helpers/picoCss.js";
 
 export interface settings
 {
@@ -13,38 +13,49 @@ export function createSettings(onSave: (val: settings) => void, preVal: settings
 {
   let mod = create('dialog', document.body);
 
-  let a = create('article', mod);
+  let node = create('article', mod);
 
-  createButton('', () => mod.open = false, a, false, 'close', 'secondary');
+  createButton('', () => mod.open = false, node, false, 'close', 'secondary');
 
-  let oCount = createSlider('Anzahl der Hindernisse', (x) => { }, 5, 50, a);
+  let setObstc = createAcordion('Hindernisse', node);
+
+  let oCount = createSlider('Anzahl der Hindernisse', (x) => { }, 5, 50, setObstc);
   oCount.setValue(preVal.obstacleCount);
-  let oSize = createSlider('Breite der Hindernisse', (x) => { }, 50, 500, a);
+  let oSize = createSlider('Breite der Hindernisse', (x) => { }, 50, 500, setObstc);
   oSize.setValue(preVal.obstaclewitdth);
 
-  create('input', a);
-  let g = create('footer', a);
+  let setRatio = createAcordion('Spielfeld', node);
+  let heigh = createSlider('Höhe', renderAndCalculateRatio, 1, 16, setRatio);
+  let width = createSlider('Breite', renderAndCalculateRatio, 1, 16, setRatio);
+  let p = create('p', setRatio);
+
+  function renderAndCalculateRatio()
+  {
+    let h = heigh.value();
+    let w = width.value();
+    p.innerText = `Seitenverhältniss: ${h}:${w}`;
+    if (h > w)
+    {
+      let aw = Math.floor(1024 * w / h);
+      oSize.max(aw);
+      return Vektor.create(aw, 1024);
+    }
+    else
+    {
+      let ah = Math.floor(1024 * h / w);
+      oSize.max(1024);
+      return Vektor.create(1024, ah);
+    }
+  }
+  let setKeys = createAcordion('Tasten', node);
+
+  let g = create('footer', node);
   createButton('Abbrechen', () => mod.open = false, g, false, 'secondary');
   createButton('Anwenden', () =>
   {
     mod.open = false;
-    onSave({ ...preVal, ...{ obstaclewitdth: oSize.value(), obstacleCount: oCount.value() } });
+    onSave({ obstaclewitdth: oSize.value(), obstacleCount: oCount.value(), ratio: renderAndCalculateRatio() });
   }, g
   );
   return mod;
-}
-
-function createObstacleSlider(a: HTMLElement, val: number)
-{
-  let l = create('label', a);
-  l.setAttribute('for', 'obstacles');
-  let s = create('input', a);
-  s.type = 'range';
-  s.id = 'obstacles';
-  s.min = '5';
-  s.max = '50';
-  s.value = val + '';
-  l.innerText = 'Anzahl der Hindernisse ' + s.value;
-  s.oninput = () => { l.innerText = 'Anzahl der Hindernisse ' + s.value; };
-  return s;
 }
